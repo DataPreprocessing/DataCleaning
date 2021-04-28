5ba6a0c99583ed8f556994a734cc4814e55f2c04
Indu <i.ramamurthy@joovence.com>
2021-04-28 22:42:41 +0200

    v1.0 setup

--- setup.py
+++ setup.py
@@ -1,17 +1,20 @@
 import setuptools
+import os
+import sys
+here = os.path.abspath(os.path.dirname(__file__))
 
-with open("README.md", "r") as fh:
+
+with open(os.path.join(here, "README.md"), "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
      name='data-cleaning',
      version='1.0',
-     scripts=['datacleaning'] ,
      author="Murali krishna mopi devi, Induraj P.Ramamurthy",
      author_email="mopidevi@gmail.com, induraj.gandhian@yahoo.com",
      description="An utility to clean the data and return you the cleaned data",
      long_description=long_description,
-   long_description_content_type="text/markdown",
+     long_description_content_type="text/markdown",
      url="https://github.com/DataPreprocessing/DataCleaning",
      packages=setuptools.find_packages(),
      classifiers=[
@@ -20,8 +23,11 @@ setuptools.setup(
          'License :: OSI Approved :: MIT License',
          'Natural Language :: English',
          'Operating System :: OS Independent',
+         'Programming Language :: Python :: 3.6',
          'Programming Language :: Python :: 3.7',
+         'Programming Language :: Python :: 3.8',
          'Topic :: Software Development :: Libraries :: Python Modules',
          'Topic :: Scientific/Engineering :: Artificial Intelligence',
      ],
+    python_requires=">=3.6"
  )
